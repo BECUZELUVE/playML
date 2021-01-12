6a0b92c21bdb53bf24e1024e448134e9b8607e79
BECUZELUVE <477791584@qq.com>
2021-01-12 23:03:34 +0800

    简单线性回归,r2评估算法效果

--- 线性回归算法/SimpleLinearRegression.py
+++ 线性回归算法/SimpleLinearRegression.py
@@ -3,6 +3,7 @@
 # Data: 2021/1/11 23:20
 
 import numpy as np
+from metrics import r2_score
 
 class SimpleLinearRegression1:
 
@@ -83,3 +84,12 @@ class SimpleLinearRegression2:
     def _predict(self,x_single):
         '''给定单个待预测数据x_single，返回x的预测结果值'''
         return self.a_*x_single+self.b_
+
+    # 因为sklearn中的线性回归算法的score返回的是r方的结果
+    def score(self,x_test,y_test):
+        '''根据测试数据集x_test和y_test确定当前模型的准确度'''
+        y_predict = self.predict(x_test)
+        return r2_score(y_test,y_predict)
+
+    def __repr__(self):
+        return "SimpleLinearRegression"
